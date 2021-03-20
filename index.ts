cddb15fc2e902f87e2ac88c9386d261bc963e9b7
Marcelo Zapaia <zapaiamarce@gmail.com>
2021-03-20 16:10:12 -0300

    extendiendo clases

---
+++ index.ts
@@ -0,0 +1,29 @@
+class ListaDeCosas {
+  name: string;
+  cosas: any[] = [];
+  constructor(name: string) {
+    // nombre de esta lista
+    this.name = name;
+  }
+  add(nuevaCosa) {
+    this.cosas.push(nuevaCosa);
+  }
+  getCosas() {
+    return this.cosas;
+  }
+}
+
+class Product {
+  name: string;
+  price: number;
+  id: number;
+  constructor(name: string, price: number, id: number) {
+    this.name = name;
+    this.price = price;
+    this.id = id;
+  }
+}
+
+class ListaDeProductos extends ListaDeCosas {}
+
+export { ListaDeProductos, Product };
