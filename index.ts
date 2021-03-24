8e2572d8790d66a7f20d866b246eb196bff006ba
Gonzalo Cabrera <gdcabrera93@gmail.com>
2021-03-24 19:20:23 -0300

    Desafio herencia y lodash

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as _ from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,42 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const productosDelArchivo = JSON.parse(
+      fs.readFileSync(__dirname + "/products.json").toString()
+    );
+    _.forEach(productosDelArchivo, (prod: Product) => {
+      this.addProduct(prod);
+    });
+  }
+
+  addProduct(pr: Product) {
+    const prId = pr.id;
+    if (!_.some(this.cosas, { id: prId })) {
+      this.add(pr);
+    }
+  }
+
+  getProduct(id: number): Product {
+    return _.find(this.cosas, function (product: Product) {
+      return product.id == id;
+    });
+  }
+
+  removeProduct(id: number) {
+    return _.remove(this.cosas, (c) => c.id == id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    if (order == "asc") {
+      return _.sortBy(this.cosas, ["price"]);
+    }
+    if (order == "desc") {
+      return _.reverse(_.sortBy(this.cosas, ["price"]));
+    }
+    return this.cosas;
+  }
+}
 
 export { ListaDeProductos, Product };
