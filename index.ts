61344c9eb778186b9dee5f2084b82b881ed48844
Sofia <sofiavign@gmail.com>
2021-03-24 14:42:16 -0300

    Clases y Metodos Completados

--- index.ts
+++ index.ts
@@ -1,8 +1,17 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
+function recibirDatos() {
+  const archivo = fs.readFileSync(__dirname + "/products.json");
+  const productos = JSON.parse(archivo.toString());
+  return productos;
+}
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
   constructor(name: string) {
-    // nombre de esta lista
     this.name = name;
   }
   add(nuevaCosa) {
@@ -24,6 +33,35 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const productos = recibirDatos();
+    productos.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(unProducto: Product) {
+    const idIgual = this.cosas.find((c) => {
+      return unProducto.id == c.id;
+    });
+    if (!idIgual) {
+      this.add(unProducto);
+    }
+  }
+  getProduct(id: number): Product {
+    return this.cosas.find((c) => {
+      return c.id == id;
+    });
+  }
+  removeProduct(id: number): Product {
+    return remove(this.cosas, (p) => {
+      return id == p.id;
+    });
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
