80edb08d0b443c53d066c0b8eb81465289ef8848
Pablo Murillo <pablomurillo.sp@gmail.com>
2021-03-28 18:19:50 -0300

    primer dia del ejercicio

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -6,7 +8,7 @@ class ListaDeCosas {
     this.name = name;
   }
   add(nuevaCosa) {
-    this.cosas.push(nuevaCosa);
+    this.cosas.concat(nuevaCosa);
   }
   getCosas() {
     return this.cosas;
@@ -17,13 +19,38 @@ class Product {
   name: string;
   price: number;
   id: number;
-  constructor(name: string, price: number, id: number) {
+  constructor(id: number, name: string, price: number) {
     this.name = name;
     this.price = price;
     this.id = id;
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const contenidoArchivo = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const productosDelArchivo = JSON.parse(contenidoArchivo);
+  }
+
+  addProduct(product: Product) {
+    this.add(product);
+  }
+  getProduct(id: number) {
+    const res = this.cosas.find((p) => {
+      return p.id == id;
+    });
+    return res;
+  }
+  removeProduct(id: number) {
+    const productoAEliminar = this.cosas.find((p) => {
+      return p.id == id;
+    });
+    this.cosas; // aca elimininamos el producto del la lista
+  }
+  getSortedByPrice() {}
+}
 
 export { ListaDeProductos, Product };
