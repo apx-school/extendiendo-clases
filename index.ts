40c9b51446be8ac6da4cbf01fd5bd3ad664c1fcd
Alex Bal <alexenri12@gmail.com>
2021-03-28 21:17:54 -0300

    pasaron los test

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as orderBy from "lodash/orderBy";
+const fs = require("fs");
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,40 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const archivo = fs.readFileSync(__dirname + "/products.json");
+    const productos = JSON.parse(archivo);
+    for (let i = 0; i < productos.length; i++) {
+      const element = productos[i];
+      this.addProduct(element);
+    }
+  }
+  // cosas: Product[] = [];
+  addProduct(producto: Product) {
+    this.add(producto);
+  }
+  getProduct(id: number): Product {
+    const productoBuscado = this.cosas.find((i) => {
+      return i.id == id;
+    });
+    return productoBuscado;
+  }
+  removeProduct(id: number) {
+    this.cosas.splice(0, id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    console.log(order);
+    var arrayOrd: Product[] = [];
+    if (order == "asc") {
+      arrayOrd = orderBy(this.cosas, "price", "asc");
+      console.log(arrayOrd);
+    } else {
+      arrayOrd = orderBy(this.cosas, "price", "desc");
+    }
+    return arrayOrd;
+  }
+}
 
 export { ListaDeProductos, Product };
