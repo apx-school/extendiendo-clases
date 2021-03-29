fcf9c7f4cdbb33b103f67c2a5d3cd0773e1e266b
FacundoEG <F_E_G.93@hotmail.com>
2021-03-28 23:40:17 -0300

    Commit de la resolución del desafío

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as products from "./products.json"
+import * as _ from "lodash"
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,58 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(name:string){
+    super(name)
+    
+    products.map(producto => {
+      this.addProduct(producto)
+      
+    });
+    
+  }
+
+  
+  addProduct(Product:Product){
+  
+    const idDeLosProductos = this.cosas.filter(item => {item.id});
+    
+    if (idDeLosProductos.includes(Product.id)){
+      throw "El id del producto que ingresaste ya existe."
+    } 
+  
+    this.add(Product)
+  }
+  
+
+getProduct(idNumber:number):Product{
+ const resultadofiltrado = _.find(this.cosas,['id', idNumber])
+ return resultadofiltrado
+}
+
+
+removeProduct(idNumber:number){
+const productoABorrar = _.find(this.cosas,['id', idNumber]) 
+_.pull(this.cosas,productoABorrar)
+}
+
+
+getSortedByPrice(order:string){
+
+  type order = "asc" | "desc"
+
+ if(order == "asc") {
+ return this.cosas = _.orderBy(this.cosas,"name","asc")
+ }
+
+
+if(order == "desc") {
+  return this.cosas = _.orderBy(this.cosas,"name","desc")
+
+}}
+
+}
+
 
 export { ListaDeProductos, Product };
