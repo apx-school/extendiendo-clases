77a4f37e2a901e21a333e3383ea3262679aedf4a
Fernando de Row <ferderow@gmail.com>
2021-04-04 16:48:12 -0300

    Se crearon las funciones addProduct, removeProduct y getSortBy y se extendio el constructor para que lea los datos de la DB

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,47 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    //Lee el archivo products.JSON y lo convierte a texto
+    const datosRecibidos = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    //Transformo los datos recibidos en formato string a objetos.
+    const datosParseados = JSON.parse(datosRecibidos);
+    //Itero el array creado y guardo el valor (en este caso un objeto) de cada una de las posiciones del array
+    //en "ListaDeCosas" mediante el metodo addProduct
+    datosParseados.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  //Este metodo recibe como parametro una instancia de Product
+  //y utiliza el metodo .add de la clase padre (ListaDeCosas) para guardar en la misma los productos.
+  addProduct(producto: Product) {
+    this.add(producto);
+  }
+
+  getProduct(id: number) {
+    const arrayDeCosas = this.getCosas();
+    const encontrado = arrayDeCosas.find((item) => {
+      return item.id == id;
+    });
+    return encontrado;
+  }
+
+  removeProduct(id: number) {
+    const arrayDeCosas = this.getCosas();
+    const arrayConProductoRemovido = remove(
+      arrayDeCosas,
+      (producto) => producto.id == id
+    );
+  }
+
+  getSortedByPrice(order: "asc" | "desc") {
+    const arrayDeCosas = this.getCosas();
+    return orderBy(arrayDeCosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
