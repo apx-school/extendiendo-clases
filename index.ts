1c6332c562d8047ae78fa89b5fef37b7eea0e833
EmerMG <emerg04@gmail.com>
2021-04-13 23:14:43 -0300

    created class ListaDeProductos()

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as products from "./products.json";
+import * as fs from "fs" 
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,40 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name)
+    const readJSON = fs.readFileSync(__dirname + "/products.json").toString();
+    const parseJSON = JSON.parse(readJSON);
+    parseJSON.forEach((i) => {
+      this.addProduct(i)
+    });
+     
+  }
+  
+  addProduct(instance:Product){
+    if(this.cosas.includes(i => i.id = instance.id)){}
+    else this.add(instance);
+  }
+
+  getProduct(id:number):Product{
+    var aux = this.getCosas();
+    var aux2 = aux.find(i => i.id === id);
+    return aux2
+  }
+
+  removeProduct(id:number){
+    var aux = this.cosas.indexOf(i => i.id = id)
+    this.cosas.splice(aux, 1); 
+  }
+
+  getSortedByPrice(order:string){
+    type order = "asc" | "desc";
+    if(order === "asc"){return this.cosas.sort(
+      (a, b) => (a.price < b.price ? -1 : 1))}
+    else{return this.cosas.sort(
+      (a, b) => (a.price > b.price ? -1 : 1))}  
+  }
+} 
 
 export { ListaDeProductos, Product };
