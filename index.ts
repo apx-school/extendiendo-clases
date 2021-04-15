a2e81356e303a740f02f0ffb72b7fa367ada92eb
Maximo <maximorossini2016@gmail.com>
2021-04-15 13:14:23 -0300

    cambios aplicados

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,30 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name);
+    const jsonFileContentReader = fs.readFileSync(__dirname + "/products.json").toString();
+    const parsedContent = JSON.parse(jsonFileContentReader);
+    parsedContent.forEach((p)=>{
+      this.addProduct(p);
+    })
+  };
+  addProduct(product:Product){
+    this.add(product);
+  };
+  getProduct(id:number){
+    const productFinder = this.cosas.find((p) => p.id == id);
+    return productFinder;
+  };
+  removeProduct(id:number){
+    const productRemover = remove (this.cosas,(p) => p.id == id);
+    return productRemover;
+  };
+  getSortedByPrice(order : "asc" | "desc"){
+    const orderedProducts = orderBy(this.cosas,["price"],[order]);
+    return orderedProducts;
+  };
+}
 
 export { ListaDeProductos, Product };
