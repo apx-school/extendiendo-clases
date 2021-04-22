d1240f9edb4980fa5c0c133df803a8015a80abb7
Alan Lipschutz <alanelipschutz@gmail.com>
2021-04-22 11:29:54 -0300

    extendí la listaDeProductos, agregué las funciones extensivas de add, get, remove y sort

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs";
+import { dirname } from "node:path";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +29,56 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  stockProductos: Product[];
+  constructor(name: string) {
+    super(name);
+    const stockProductos = JSON.parse(
+      fs.readFileSync(__dirname + "/products.json").toString()
+    );
+    stockProductos.forEach((producto) => {
+      this.addProduct(producto);
+    });
+  }
+
+  addProduct(producto: Product) {
+    let productoEncontrado = this.cosas.find((item) => {
+      return producto.id == item.id;
+    });
+    if (!productoEncontrado) {
+      //no usar aca productoEncontrad == undefined, usar !productoEncontrdo para definir que no lo encontrás
+      this.add(producto);
+    }
+  }
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((item) => {
+      return item.id == id;
+    });
+  }
+
+  removeProduct(id: number) {
+    remove(this.cosas, (item) => {
+      return item.id == id;
+    });
+    return this.cosas;
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
+
+// function main() {
+//   const listadoDeProductos = new ListaDeProductos("productos");
+//   const procesadora = new Product("procesadora", 10, 1);
+//   const batidora = new Product("batidora", 10, 2);
+//   const lavarropas = new Product("lavarropas", 10, 6);
+//   listadoDeProductos.addProduct(procesadora);
+//   listadoDeProductos.addProduct(batidora);
+//   listadoDeProductos.addProduct(lavarropas);
+//   console.log(listadoDeProductos);
+// }
+
+// main();
