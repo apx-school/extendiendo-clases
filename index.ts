7305d228861dc3022c2aeb3a466d1548789916a7
Ezegrandia <ezequielgrandia@gmail.com>
2021-04-25 20:03:51 -0300

    Desafio completado 5 tests passed

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as sortBy from "lodash/sortBy";
+import * as reverse from "lodash/reverse";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +29,43 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+
+    const jsonToString = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const listadoDeProductos = JSON.parse(jsonToString);
+    listadoDeProductos.forEach((item) => this.addProduct(item));
+  }
+  addProduct(newProduct: Product) {
+    var yaExiste: boolean;
+    if (this.cosas.find((item) => item == newProduct)) {
+      yaExiste = true;
+    } else {
+      yaExiste = false;
+    }
+    if (yaExiste == false) {
+      this.add(newProduct);
+    }
+  }
+  getProduct(id: number): Product {
+    const productoEnconstrado = this.cosas.find((item) => item.id == id);
+    return productoEnconstrado;
+  }
+  removeProduct(id: number) {
+    remove(this.cosas, (item) => item.id == id);
+  }
+  getSortedByPrice(order: string) {
+    const listaAscendente = sortBy(this.cosas, "id");
+    const listaDescendente = reverse(listaAscendente);
+    if (order == "asc") {
+      return listaAscendente;
+    } else {
+      return listaDescendente;
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
