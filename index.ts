4dd2b87980c08e67b9d6923ad13d84d47fdb380e
AlexMuñoz <munozmiguelalexis@gmail.com>
2021-05-03 12:46:45 -0300

    desafio extendiendo-clases

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,31 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const contenidoDelArchivo = fs.readFileSync(__dirname + "/products.json").toString();
+    const productosDelArchivo = JSON.parse(contenidoDelArchivo);
+
+    productosDelArchivo.forEach((e) => {
+      this.addProduct(e);
+    });
+  }
+
+  addProduct(product: Product) {
+    this.add(product);
+  }
+  getProduct(id: number): Product {
+    return this.getCosas().find(e => e.id == id);
+  }
+  removeProduct(id: number) {
+    remove(this.cosas, (e) => e.id == id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    // hardcode | harcodeo significa escribir un valor que deveria ser una variable escrito a mano 
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
+
