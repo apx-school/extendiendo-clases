db1ed88ba0fe2a04c2e39244ef444b6eabac360e
lautaro <lautarogomez68@gmail.com>
2021-05-03 21:49:40 -0300

    test superado

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,31 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name);
+    const contenidoDeJson = fs.readFileSync(__dirname + "/products.json").toString();
+    const productosDeJson = JSON.parse(contenidoDeJson);
+    productosDeJson.forEach(p => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(product: Product){
+    this.add(product);
+  }
+  getProduct(id:number):Product {
+    const productos = this.getCosas();
+    return productos.find((p) =>(p.id == id));
+  }
+  removeProduct(id:number):Product {
+    const productoRemovido = remove(this.cosas, (p) => p.id ==id);
+    return productoRemovido; 
+  }
+  getSortedByPrice(order:string){
+    const productosOrdenados = orderBy(this.cosas, ["price"], [order]);
+    return productosOrdenados;
+  }
+  
+}
 
 export { ListaDeProductos, Product };
