12c9e60a35c3f060838168ae5b309d4cf16dd597
cristian <cris.are.3496@gmail.com>
2021-05-13 14:12:40 -0300

    Desafio completado.

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as products from "./products.json";
+import * as _ from "lodash";
+import * as fs from "fs";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,33 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(name:string){
+    super(name); //Me traigo las propiedades de la clase padre.
+    products.forEach(t=> { //"products" es del import.
+      this.add(t); //Cargo el JSON en el array original.
+    })
+  }
+
+  addProduct(product:Product){
+    this.add(product);
+  }//Fin del addProduct. (Funciona).
+  
+  getProduct(id:number):Product{
+    const array = this.getCosas();
+    const resultado = array.find((t) => t.id == id);
+    return resultado;
+  }//Fin del getProduct. (Funciona).
+
+  removeProduct(id:number){
+    _.remove(this.cosas, (t) => t.id == id);//Metoodo con Lodash.
+  }//Fin del remove. (Funciona).
+
+  getSortedByPrice(order: "asc" | "desc"){ //
+    return _.orderBy(this.cosas, ["price"], [order]);
+  }//Fin del sort. (Funciona).
+
+}
 
 export { ListaDeProductos, Product };
