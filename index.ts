62a58fdd0bfda7234149236dfd27d5a65967105d
Gianni Furci <giannifu93@gmail.com>
2021-05-14 19:33:58 -0300

    desafío terminado

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as _ from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,35 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
-
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const contenidoDelArchivo = fs
+      .readFileSync(__dirname + "/package.json")
+      .toString();
+    const productosDelArchivo = JSON.parse(contenidoDelArchivo);
+    productosDelArchivo.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(producto: Product) {
+    this.add(producto);
+  }
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((c) => c.id == id);
+  }
+  removeProduct(id: number) {
+    return _.remove(this.cosas, (c) => c.id == id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    if (order == "asc") {
+      return _.sortBy(this.cosas, ["price"]);
+    }
+    if (order == "desc") {
+      return _.reverse(_.sortBy(this.cosas, ["price"]));
+    }
+    return this.cosas;
+  }
+}
 export { ListaDeProductos, Product };
