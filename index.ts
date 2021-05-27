0abcaf4095f5db5edea1e49fceff6beb04e667ce
Lorenzo <lorenzoburgi@gmail.com>
2021-05-26 23:03:14 -0300

    finalizado

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderby";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,34 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const require = fs.readFileSync("products.json").toString();
+    const parseo = JSON.parse(require);
+    parseo.forEach((element) => {
+      this.add(element);
+    });
+  }
+  addProduct(producto: Product) {
+    this.add(producto);
+  }
+  getProduct(id: number): Product {
+    const productos = this.cosas;
+    return productos.find((cosa) => cosa.id == id);
+  }
+  removeProduct(id: number): Product {
+    const productoEncontrado = this.cosas.find((cosa) => cosa.id == id);
+    return remove(this.cosas, productoEncontrado);
+  }
+
+  getSortedByPrice(order: string) {
+    if (order == "asc") {
+      return orderBy(this.cosas, ["price"], ["asc"]);
+    } else {
+      return orderBy(this.cosas, ["price"], ["desc"]);
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
