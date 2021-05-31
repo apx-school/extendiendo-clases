24b6df4dfd66f215530ef0daec2a6eddfff44a09
Ricardo <richard.mdq@gmail.com>
2021-06-01 00:11:00 +0100

    5 test passed

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as orderBy from "lodash/orderBy";
+import * as remove from "lodash/remove";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,42 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const archivoOriginal = fs.readFileSync(__dirname + "/products.json");
+    const archivoString = archivoOriginal.toString();
+    const archivoDatos = JSON.parse(archivoString);
+    archivoDatos.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(unProducto: Product) {
+    var elProducto = this.cosas.find(function (item: any) {
+      if (item.id == unProducto.id) {
+        return unProducto;
+      }
+    });
+    if (elProducto != unProducto) {
+      this.add(unProducto);
+    }
+  }
+
+  getProduct(id: number): Product {
+    var idProducto = this.cosas.find(function (item: any) {
+      if (item.id == id) {
+        return item;
+      }
+    });
+    return idProducto;
+  }
+  removeProduct(id: number): Product {
+    return remove(this.cosas, (p) => p.id == id);
+  }
+  getSortedByPrice(order: string) {
+    type order = "asc" | "desc";
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
