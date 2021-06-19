8f7f4022fa88eacee8c52ae5e7925f873a8d7e9d
heberjbaez <heberjbaez@gmail.com>
2021-06-19 04:39:02 -0300

    desafio test herencia-lodash (extendiendo clases)

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs"
+import * as remove from "lodash/remove"
+import * as orderBy from "lodash/orderBy"
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,32 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const productsJson = fs.readFileSync(__dirname + "/products.json").toString();
+    const productsJsonParseado = JSON.parse(productsJson);
+    productsJsonParseado.forEach((element) => {
+      this.addProduct(element);
+    });
+  }
+
+  addProduct(product: Product) {
+    this.add(product)
+  }
+
+  getProduct(id: number): Product {
+    return this.getCosas().find(things => things.id == id);
+  }
+
+  removeProduct(id: number) {
+    remove(this.cosas, things => things.id == id) //es parecido a filter pero en vez de filtrar directamente remueve ese elemento
+  }
+
+  getSortedByPrice(order: "asc" | "desc") {
+    //hardcode: escribir un valor que deberia ser una varible pero escrita a mano
+    return orderBy(this.cosas, ["price"], [order])
+  }
+}
 
 export { ListaDeProductos, Product };
