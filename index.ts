0a2890b8def642dbca9fe52e1b662a2c9a8338fd
Julieta <agosx_@hotmai.com>
2021-06-28 16:47:32 -0300

    modificaciones en index.ts

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,33 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const archivoJson = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const productosDelJson = JSON.parse(archivoJson);
+    productosDelJson.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(product: Product) {
+    this.add(product);
+  }
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    const productoEncontrado = cosas.find((c) => c.id == id);
+    return productoEncontrado;
+  }
+  removeProduct(id: number): Product {
+    const arrayRemove = remove(this.cosas, (c) => c.id == id);
+    return arrayRemove;
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    const ordenado = orderBy(this.cosas, ["price"], [order]);
+    return ordenado;
+  }
+}
 
-export { ListaDeProductos, Product };
+export {ListaDeProductos, Product};
