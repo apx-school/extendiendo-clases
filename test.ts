bdfe5c031aa398ca7b6d47c5aa974c104e1abff7
Leandro <leandrom.roldan@gmail.com>
2021-07-15 20:57:47 -0300

    mètodos definidos y test passed

--- test.ts
+++ test.ts
@@ -1,41 +1,42 @@
 import test from "ava";
 import { ListaDeProductos } from "./index";
+import { Product } from "./index";
 import * as products from "./products.json";
 import * as orderBy from "lodash/orderBy";
 
 test("Testeo el constructor", (t) => {
-  const lista = new ListaDeProductos("marce");
-  t.is(lista.name, "marce");
+	const lista = new ListaDeProductos("marce");
+	t.is(lista.name, "marce");
 });
 
 test("Testeo que el constructor cargue el products.json", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const cosas = lista.getCosas();
+	const lista = new ListaDeProductos("marce");
+	const cosas = lista.getCosas();
 
-  t.deepEqual(cosas, products);
+	t.deepEqual(cosas, products);
 });
 
 test("Testeo el addProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const myP = { price: 33, id: 123, name: "mi producto" };
-  lista.addProduct(myP);
-  const myP2 = lista.getProduct(myP.id);
-  t.deepEqual(myP2, myP);
+	const lista = new ListaDeProductos("marce");
+	const myP = { price: 33, id: 123, name: "mi producto" };
+	lista.addProduct(myP);
+	const myP2 = lista.getProduct(myP.id);
+	t.deepEqual(myP2, myP);
 });
 
 test("Testeo el removeProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const myP = { price: 33, id: 123, name: "mi producto" };
-  lista.addProduct(myP);
-  lista.removeProduct(myP.id);
-  const p = lista.getProduct(myP.id);
-  t.falsy(p);
+	const lista = new ListaDeProductos("marce");
+	const myP = { price: 33, id: 123, name: "mi producto" };
+	lista.addProduct(myP);
+	lista.removeProduct(myP.id);
+	const p = lista.getProduct(myP.id);
+	t.falsy(p);
 });
 
 test("Testeo el getSortedByPrice", (t) => {
-  const lista = new ListaDeProductos("marce");
-  t.deepEqual(
-    orderBy(lista.cosas, "price", "desc"),
-    lista.getSortedByPrice("desc")
-  );
+	const lista = new ListaDeProductos("marce");
+	t.deepEqual(
+		orderBy(lista.cosas, "price", "desc"),
+		lista.getSortedByPrice("desc")
+	);
 });
