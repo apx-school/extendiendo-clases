282c2fcaa61412b0c4fe2e989bb4734c5c63df21
ariel <arieldaich@gmail.com>
2021-07-21 00:29:03 -0300

    primer commit

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as products from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,47 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas{
+  addProduct(newProduct: Product){
+    if (this.cosas.some(c => c.id == newProduct.id) == false) {
+      this.add(newProduct);
+    }
+  }
+  constructor(name: string){
+    super(name);
+    products.forEach(p => this.addProduct(p));
+  }
+  getProduct(id: number): Product {
+    return this.cosas.find(c => c.id == id);
+  }
+  removeProduct(id: number) {
+    this.cosas.splice(this.cosas.indexOf(this.cosas.find(c => c.id == id)), 1);
+  }
+  getSortedByPrice(order: "asc" | "desc"){
+    if (order = "asc") {
+      return this.cosas.sort((a, b) => {
+        if (a.id > b.id) {
+          return -1;
+        }
+        if (a.id < b.id) {
+          return 1;
+        }
+        return 0;
+      });
+    };
+    if (order = "desc") {
+      return this.cosas.sort((a, b) => {
+        if (a.id < b.id) {
+          return -1;
+        }
+        if (a.id > b.id) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
+
