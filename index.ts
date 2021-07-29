df247bd3ce45a32ae45dead32781acac65588828
Eugenio <eugeniolgimenez@gmail.com>
2021-07-29 08:57:50 -0300

    realice desafio de la Leccion 8

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -13,6 +17,57 @@ class ListaDeCosas {
   }
 }
 
+class ListaDeProductos extends ListaDeCosas {
+  /*
+  extender el constructor original para que además de recibir un nombre para la lista, 
+  lea el archivo products.json 
+  y agregue todos los productos del JSON usando el método addProduct.*/
+
+  constructor(name: string) {
+    super(name);
+
+    const jsonString = fs.readFileSync(__dirname + "/products.json").toString();
+    const jsonArray = JSON.parse(jsonString);
+
+    jsonArray.forEach((i) => {
+      this.addProduct(i);
+    });
+  }
+
+  /* 
+  un método addProduct que reciba una instancia de la clase Product como parámetro 
+  y la agregue usando el método add que ya existe en la superclase.
+  
+  El método debe validar que no exista un producto con el mismo id antes de agregarlo.
+   */
+
+  addProduct(producto: Product) {
+    this.add(producto);
+  }
+
+  /*
+  tener un método getProduct(id:number):Product que devuelva el producto con ese id.*/
+
+  getProduct(id: number) {
+    return this.cosas.find((i) => i.id == id);
+  }
+
+  /*
+  tener un método removeProduct(id:number):Product que elimine el producto con ese id.*/
+
+  removeProduct(id: number) {
+    return remove(this.cosas, (i) => i.id == id);
+  }
+
+  /*
+  tener un método getSortedByPrice que reciba un parámetro order:string con solo dos valores posibles: asc o desc.
+  */
+
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
+
 class Product {
   name: string;
   price: number;
@@ -23,7 +78,5 @@ class Product {
     this.id = id;
   }
 }
-
-class ListaDeProductos extends ListaDeCosas {}
 
 export { ListaDeProductos, Product };
