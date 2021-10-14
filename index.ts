8aec1bf0187e9a5c62476d3abd626e20e3e54122
Javier Diaz <diazjavierdavid@hotmail.com>
2021-10-13 21:40:18 -0300

    fin del ejercicio

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as _ from "lodash";
+import { readFileSync } from 'fs';
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,43 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(name: string){
+    super(name);
+    const data: Product[] = JSON.parse(readFileSync(`${__dirname}/products.json`).toString());
+    data.forEach(producto => this.addProduct(producto));
+  }
+  
+  addProduct(producto: Product){
+    const seRepite = this.cosas.find(cosa => {
+      return cosa.id === producto.id;
+    })
+    
+    if(!seRepite){
+      this.add(producto);
+    }
+  }
+
+  getProduct(id:number){
+    const producto = this.cosas.find(cosa => {
+      return cosa.id === id;
+    })
+
+    return producto;
+  }
+
+  removeProduct(id: number){
+    const nuevoArray = this.cosas.filter(cosa => {
+        cosa.id !== id;
+    })
+
+    this.cosas = nuevoArray;
+  }
+
+  getSortedByPrice(order: "asc" | "desc"){
+    return _.orderBy(this.cosas, ['price'],[order]);
+  }
+}
 
 export { ListaDeProductos, Product };
