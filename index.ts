7d557897dc571237ad706307fb2a14996da32f1f
Lucas Bastidas <lucaasbastidas@hotmail.com>
2021-11-15 09:56:33 -0300

    desafio terminado

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+import * as fs from "fs";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
   constructor(name: string) {
     // nombre de esta lista
     this.name = name;
+
   }
   add(nuevaCosa) {
     this.cosas.push(nuevaCosa);
@@ -24,6 +29,34 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+ constructor(name: string){
+   super(name)
+   var productosJson = fs.readFileSync(__dirname + "/products.json").toString();
+   var listaProductos = JSON.parse(productosJson);
+   listaProductos.forEach(element => {
+     this.addProduct(element)
+   });
 
-export { ListaDeProductos, Product };
+ }
+  addProduct(producto: Product){
+  this.add(producto)
+}
+  getProduct(id:number):Product {
+    var cosas = this.getCosas()
+    return cosas.find(function(item){return item.id === id})
+  }
+  removeProduct(id:number):Product{
+    let productoARemover = this.cosas.find(function(item){return item.id === id})
+    return remove(this.cosas, function(n){
+      return n === productoARemover
+    })
+  }
+  getSortedByPrice(order:"asc" | "desc"){
+    return orderBy(this.cosas, ["price"],[order])
+  }
+
+  }
+
+export { ListaDeProductos, Product, ListaDeCosas };
+
