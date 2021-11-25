63f4f65ea0bc4106e97cd88d601b1f0541034ec2
Dylan <dylanfp14@gmail.com>
2021-11-24 21:54:29 -0300

    desafio finalizado

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
 
 class ListaDeCosas {
   name: string;
@@ -30,7 +32,7 @@ class ListaDeProductos extends ListaDeCosas {
   constructor(name: string) {
     super(name);
     const contenidoDelArchivo = fs
-      .readFileSync(__dirname + "./products")
+      .readFileSync(__dirname + "/products.json")
       .toString();
     const productosDelArchivo = JSON.parse(contenidoDelArchivo);
     productosDelArchivo.forEach((p) => {
@@ -45,8 +47,13 @@ class ListaDeProductos extends ListaDeCosas {
     const cosas = this.getCosas();
     return cosas.find((c) => c.id == id);
   }
-  // // removeProduct() {}
-  // getSortedByPrice() {}
+  removeProduct(id: number) {
+    return remove(this.cosas, (c) => c.id == id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    //hardcode | harcodeo es el valor escrito a mano, en vez de recibir un parámetro flexible
+    return orderBy(this.cosas, ["price"], [order]);
+  }
 }
 
 export { ListaDeProductos, Product };
