f0945c843d5e17a28edd04dbcc6d085f180803c3
xtremonius <alejandro.aquino1988@gmail.com>
2021-12-20 13:57:34 -0300

    desafio terminado

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import { constants } from "buffer";
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +29,34 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const contenidoArchivo = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const productosDelArchivo = JSON.parse(contenidoArchivo);
+    productosDelArchivo.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+
+  addProduct(product: Product) {
+    this.add(product);
+  }
+
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((c) => c.id == id);
+  }
+  removeProduct(id: number) {
+    const arrayNuevo = remove(this.cosas, (c) => c.id == id);
+    //console.log(id, this.cosas, arrayNuevo);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+    //console.log(t);
+  }
+}
 
 export { ListaDeProductos, Product };
