b76b18873c21480d92e2f4b95735429490f1983b
Luciano Riente <lucianotomasriente@gmail.com>
2022-01-24 12:50:15 -0300

    Desafío terminado :D

--- index.ts
+++ index.ts
@@ -1,29 +1,71 @@
+import * as fs from 'fs'; // Nos permite traer la data actualizada del archivo con los ultimos cambios que hayamos hecho
+import * as orderBy from 'lodash/orderBy';
+
 class ListaDeCosas {
-  name: string;
-  cosas: any[] = [];
-  constructor(name: string) {
-    // nombre de esta lista
-    this.name = name;
-  }
-  add(nuevaCosa) {
-    this.cosas.push(nuevaCosa);
-  }
-  getCosas() {
-    return this.cosas;
-  }
+	name: string;
+	cosas: any[] = [];
+	constructor(name: string) {
+		this.name = name;
+	}
+	add(nuevaCosa: Product) {
+		this.cosas.push(nuevaCosa);
+	}
+	getCosas() {
+		return this.cosas;
+	}
 }
 
 class Product {
-  name: string;
-  price: number;
-  id: number;
-  constructor(name: string, price: number, id: number) {
-    this.name = name;
-    this.price = price;
-    this.id = id;
-  }
+	name: string;
+	price: number;
+	id: number;
+	constructor(name: string, price: number, id: number) {
+		this.name = name;
+		this.price = price;
+		this.id = id;
+	}
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+	constructor(name: string) {
+		super(name);
+		const datosDeProductos = fs.readFileSync(__dirname + '/products.json'); // Yo le había puesto directamente /products.json (es lo mismo)
+		const datosParseados = JSON.parse(datosDeProductos.toString());
+		datosParseados.forEach((dato: Product) => this.addProduct(dato));
+	}
+
+	addProduct(instancia: Product) {
+		const productos = this.cosas;
+		const idDeProductos = productos.find((prod) => prod.id === instancia.id);
+		if (!idDeProductos) {
+			this.add(instancia); // Es lo mismo que this.cosas.push(instancia)
+		} else {
+			throw 'Hay más de un producto con un mismo Id';
+		}
+		// this.add(instancia); // Esto solo si no se quiere ver cuando hay un id repetido
+	}
+
+	getProduct(id: number): Product {
+		const productos = this.getCosas();
+		return productos.find((prod) => prod['id'] === id);
+	}
+
+	removeProduct(id: number): Product {
+		const productos = this.cosas;
+		const productoQueSeQuiereRemover = productos.find((prod) => prod['id'] === id);
+		for (let i = 0; i < productos.length; i++) {
+			const producto = productos[i];
+			if (producto === productoQueSeQuiereRemover) {
+				this.cosas.splice(i, 1);
+			}
+		}
+		return productoQueSeQuiereRemover;
+		// Marce usó loadash ---> remove(this.cosas, () => c.id == id);
+	}
+	// Marce usó --> order: 'asc' | "desc"
+	getSortedByPrice(order: string): Product[] {
+		return orderBy(this.cosas, ['price'], [order]);
+	}
+}
 
 export { ListaDeProductos, Product };
--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
-import test from "ava";
-import { ListaDeProductos } from "./index";
-import * as products from "./products.json";
-import * as orderBy from "lodash/orderBy";
+import test from 'ava';
+import { ListaDeProductos } from './index';
+import * as products from './products.json';
+import * as orderBy from 'lodash/orderBy';
 
-test("Test de prueba", (t) => {
-  t.is("hola", "hola");
+test('Test de prueba', (t) => {
+	t.is('hola', 'hola');
 });
 
 // todos los tests que siguen van a fallar
@@ -13,39 +13,35 @@ test("Test de prueba", (t) => {
 // a medida que vayas avanzando en el objetivo
 // de cada test
 
-test("Testeo el constructor", (t) => {
-  const lista = new ListaDeProductos("marce");
-  t.is(lista.name, "marce");
+test('Testeo el constructor', (t) => {
+	const lista = new ListaDeProductos('marce');
+	t.is(lista.name, 'marce');
 });
 
-test("Testeo que el constructor cargue el products.json", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const cosas = lista.getCosas();
-
-  t.deepEqual(cosas, products);
+test('Testeo que el constructor cargue el products.json', (t) => {
+	const lista = new ListaDeProductos('marce');
+	const cosas = lista.getCosas();
+	t.deepEqual(cosas, products);
 });
 
-test("Testeo el addProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const myP = { price: 33, id: 123, name: "mi producto" };
-  lista.addProduct(myP);
-  const myP2 = lista.getProduct(myP.id);
-  t.deepEqual(myP2, myP);
+test('Testeo el addProduct', (t) => {
+	const lista = new ListaDeProductos('marce');
+	const myP = { price: 33, id: 123, name: 'mi producto' };
+	lista.addProduct(myP);
+	const myP2 = lista.getProduct(myP.id);
+	t.deepEqual(myP2, myP);
 });
 
-test("Testeo el removeProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
-  const myP = { price: 33, id: 123, name: "mi producto" };
-  lista.addProduct(myP);
-  lista.removeProduct(myP.id);
-  const p = lista.getProduct(myP.id);
-  t.falsy(p);
+test('Testeo el removeProduct', (t) => {
+	const lista = new ListaDeProductos('marce');
+	const myP = { price: 33, id: 123, name: 'mi producto' };
+	lista.addProduct(myP);
+	lista.removeProduct(myP.id);
+	const p = lista.getProduct(myP.id);
+	t.falsy(p);
 });
 
-test("Testeo el getSortedByPrice", (t) => {
-  const lista = new ListaDeProductos("marce");
-  t.deepEqual(
-    orderBy(lista.cosas, "price", "desc"),
-    lista.getSortedByPrice("desc")
-  );
+test('Testeo el getSortedByPrice', (t) => {
+	const lista = new ListaDeProductos('marce');
+	t.deepEqual(orderBy(lista.cosas, 'price', 'desc'), lista.getSortedByPrice('desc'));
 });
