d75c5dfc81823e100deca79be8490a62c99afaf8
Marcos Acuña <marcosjuako@hotmail.com>
2022-02-15 20:32:59 -0300

    agregue las funciones y pase los test

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import * as products from "./products.json";
+import * as fs from "fs";
+import * as lodash from "lodash";
+
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +29,40 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(name:string){
+    super(name);
+    this.cosas = products.map(i=>i); 
+  }
+
+  addProduct(newProduct:Product){
+    const noEsta = this.cosas.find( i => i.id == newProduct.id ); 
+    if (!noEsta){
+       this.add(newProduct);
+    }else{
+    console.log("el ID ", newProduct.id, " ya existe");
+    }
+  }
+  getProduct(id:number):Product{
+    return this.cosas.find(i => i.id == id);
+  }
+  removeProduct(id:number){
+    this.cosas = this.cosas.filter(i => i.id != id );
+  }
+  getSortedByPrice(order:string){
+    const isAscendent = order.toUpperCase() == "ASC";
+    const isDescendent = order.toUpperCase() == "DESC";
+    
+    if(isAscendent){
+      this.cosas = lodash.orderBy(this.cosas,'price', 'asc');
+    }
+    if(isDescendent){
+      this.cosas = lodash.orderBy(this.cosas, 'price', 'desc');
+    }
+    return this.cosas;
+  }
+}
+
 
 export { ListaDeProductos, Product };
