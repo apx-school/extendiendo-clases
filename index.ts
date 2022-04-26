c1d8cf43c2d9af1148837cdc026cbdb823eadd92
IvoASandoval <ivoasandoval@gmail.com>
2022-04-25 22:04:45 -0300

    se creo class ListasDeProductos y sus respectivos metodo

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +29,30 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const prodJson = fs.readFileSync(__dirname + "/products.json").toString();
+    const prodDelArchivo = JSON.parse(prodJson);
+    prodDelArchivo.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+
+  addProduct(prod: Product) {
+    return this.add(prod);
+  }
+
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((cosa) => cosa.id == id);
+  }
+  removeProduct(id: number) {
+    remove(this.cosas, (cosa) => cosa.id == id);
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
