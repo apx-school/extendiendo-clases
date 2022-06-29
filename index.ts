e2e5fa9473ff18b4ad71285f31740d08325ee71f
Guillenjulian <guille.julian1985@gmial.com>
2022-06-28 23:28:30 -0300

    se completan las funciones para realizar el agregado del paquete parce.json, a la vez se realizan las suma del parametro name, y metodo getProduct para ordenarlos por ID, removeProduct para remover los products con el mismo id y el metodo getSortedByPrice para poder ordenar por price

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,37 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    //se suma en el constructor el parametro Name
+    super(name);
+    //se agrega el products.json , se los parsea
+    const productJson = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+
+    const productsArch = JSON.parse(productJson);
+
+    productsArch.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  addProduct(product: Product) {
+    this.add(product);
+  }
+  //se genera el parametro para para ordenarlos por id
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((c) => c.id == id);
+  }
+  // se remueve los id que tiene el mismo valor utilizando Remove
+  removeProduct(id: number) {
+    remove(this.cosas, (c) => c.id == id);
+  }
+  // se ordena los precios en forma decendente utilisando orderBy
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
