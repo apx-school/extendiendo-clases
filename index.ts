a1b7279f22b0244796b06ee7c3d090c500e139b2
Carlos Govi <cgovi66@gmail.com>
2022-07-21 22:22:27 -0300

    agrego las clases y los metodos solicitados en el desafio

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as productos from "./products.json";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,35 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  //constructor de la clase con super en name
+  constructor(name: string) {
+    super(name);
+    //agrega todos los productos del json
+    productos.forEach((item) => {
+      this.addProduct(item);
+    });
+  }
+
+  //agrega un producto a la lista
+  addProduct(product: Product) {
+    //si no existe un producto con el mismo id, lo agrega
+    if (!this.getProduct(product.id)) {
+      this.add(product);
+    }
+  }
+  //getProduct devuleve el producto con id = id
+  getProduct(id: number) {
+    return this.cosas.find((item) => item.id === id);
+  }
+  //removeProduct elimina el producto con id = id
+  removeProduct(id: number) {
+    this.cosas = this.cosas.filter((item) => item.id !== id);
+  }
+  //getSortedByPrice devuelve un array de productos ordenados por precio ascendente o descendente
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, "price", order);
+  }
+}
 
 export { ListaDeProductos, Product };
