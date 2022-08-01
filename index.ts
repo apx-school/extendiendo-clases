ce3b94e2784377626619f720d100a7c43cd72fa4
Matias Cortese <matiasemanuelcortese@gmail.com>
2022-08-01 17:33:32 -0400

    Se agrega la clase ListaDeProductos

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs';
+import * as _ from 'lodash';
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,45 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string){
+    super(name);
+    const data = fs.readFileSync('./products.json').toString();
+    const dataJSON = JSON.parse(data);
+    dataJSON.forEach((object) => {
+      this.addProduct(object);
+    })
+  }
+  addProduct(product: Product) {
+    const chequearId = this.cosas.find((cosa) => {
+      return cosa.id === product.id;
+    })
+    if (chequearId == undefined){
+      this.add(product);
+    }
+    else {
+      return "A product with the same ID already exists"
+    }
+  }
+  getProduct(id: number):Product{
+    const productConId = this.cosas.find((product) => {
+        return product.id === id;
+      })
+    return productConId;
+  }
+  removeProduct(id: number):Product{
+    const indexOfObject = this.cosas.findIndex((cosa) => {
+      return cosa.id === id;
+    });
+    if (indexOfObject !== -1) {
+      this.cosas.splice(indexOfObject, 1)
+    }
+    return;
+  }
+  getSortedByPrice(order: "asc" | "desc"){
+    return _.orderBy(this.cosas, 'price', order);
+  }
+};
+
 
 export { ListaDeProductos, Product };
