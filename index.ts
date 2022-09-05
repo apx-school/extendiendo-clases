bf7e407f614c89b62cb78910784fd0d8691fd96c
Your Name <you@example.com>
2022-09-05 11:24:16 -0500

    pase los test

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,36 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const llamarLista = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const parsear = JSON.parse(llamarLista);
+    parsear.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+
+  addProduct(productoNuevo: Product) {
+    return this.add(productoNuevo);
+  }
+  getProduct(id: number): Product {
+    const buscaCosas = this.getCosas();
+    return buscaCosas.find((cosas) => 
+      cosas.id == id
+    );
+  }
+  removeProduct(id: number): Product {
+    const eliminarUnaCosa = remove(this.cosas, (i) => 
+      i.id == id);
+    return eliminarUnaCosa;
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    const resultado = orderBy(this.cosas, ["price"], order);
+    return resultado;
+  }
+}
 
 export { ListaDeProductos, Product };
