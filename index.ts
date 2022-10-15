c1e1e6c90e0a85f561510fa830612e412b6c3468
Jose Fernandez <jotaj1019@gmail.com>
2022-10-14 19:30:22 -0600

    mi primer commit

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+import * as lodash from "lodash";
+// const products = require("./products.json");
+import * as productos from "./products.json";
+import * as fs from "fs";
+import { privateDecrypt } from "crypto";
+
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +31,47 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  addProduct(producto: Product){
+    this.cosas.find(function (p) {
+      if (p.id === producto.id){
+        throw "repeated ID";  // cambiarlo?
+      } else {
+        // .add(producto);
+      }
+    });
+  };
+  constructor(name: string){
+    super(name);
+    const texto = fs.readFileSync(__dirname + "/products.json");
+    const textoParseado = JSON.parse(texto);
+    this.addProduct(textoParseado);
+  }
+  getProduct(id:number): Product{
+    // this.cosas.find((p)=> p.id === id)
+    const productoEncontrado = this.cosas.find(function(p) {
+      if (p.id === id){
+        return p;
+      }
+    })
+    return productoEncontrado;
+  };
+  removeProduct(id:number): Product{
+    const productoEncontrado2 = this.cosas.find((p)=> p.id === id);
+    return lodash.remove(productoEncontrado2); 
+  };
+  getSortedByPrice(order: "asc" | "desc"){
+    this.cosas.sort(function (a, b) {
+      if (order === "asc"){
+          return a - b;
+      } else if (order === "desc"){
+        return b - a;
+      }
+    })
+    // if (order === "asc"){
+    //   return lodash.orderBy(this.cosas(this.cosas["price", "asc"]))
+    // }
+  };
+}
 
 export { ListaDeProductos, Product };
