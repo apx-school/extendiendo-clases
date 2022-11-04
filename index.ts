7bdfe16ef41be910a225023a80c4cbc171803e75
julian123456789222 <bustillojulian209@gmail.com>
2022-11-04 00:12:24 -0300

    tsts completado

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import { readFileSync } from "fs";
+import { orderBy } from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,41 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    this.pullProducts();
+  }
+
+  pullProducts() {
+    const productos = JSON.parse(
+      readFileSync("products.json", { encoding: "utf-8" })
+    );
+    productos.forEach((e) => this.addProduct(e));
+  }
+
+  addProduct(producto: Product) {
+    if (this.cosas.length > 0) {
+      this.cosas.forEach((e) => {
+        if (e.id === producto.id)
+          throw `Ya se encuentra en la lista de productos un producto con id: ${producto.id}`;
+      });
+    }
+    this.cosas.push(producto);
+  }
+
+  getProduct(id: number) {
+    return this.cosas.find((e) => e.id === id);
+  }
+
+  removeProduct(id: number) {
+    const removeIndex = this.cosas.findIndex((e) => e.id === id);
+    this.cosas.splice(removeIndex, 1);
+  }
+
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, "price", order);
+  }
+}
 
 export { ListaDeProductos, Product };
