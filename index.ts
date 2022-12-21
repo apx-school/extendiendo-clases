5d55ff42c1549f1130ea249238a29af5176f3ada
José Ignacio Porcelo <nacho.porcelo20@gmail.com>
2022-12-21 18:06:17 -0300

    Cambios del desafio

--- index.ts
+++ index.ts
@@ -24,6 +24,37 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string){
+    super(name)
+    const productsJson = fs.readFileSync(__dirname + "/products.json").toString()
+    const productsJsonParse = JSON.parse(productsJson)
+    productsJsonParse.forEach((prod) => {
+      this.addProduct(prod)
+    });
+  }
+  addProduct(product:Product){
+    return this.add(product)
+  }
+  getProduct(id:number){
+    const productList = this.getCosas()
+    return productList.find(p=>
+      p.id == id
+    )
+    
+  }
+  removeProduct(id:number){
+    const productList = this.getCosas()
+    const productRemoved = remove(productList, p => p.id == id)
+    return productRemoved
+  }
+  getSortedByPrice(order:"asc"|"desc"){
+    const productList = this.getCosas()
+    return orderBy(productList,["price"],[order]) 
+  }
+}
 
+import * as orderBy from "lodash/orderBy"
+import * as remove from "lodash/remove"
+import * as fs from "fs"
 export { ListaDeProductos, Product };
