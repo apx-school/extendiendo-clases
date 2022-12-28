0694a59b39e8869639dd78ac125bf20fabafa907
Lautaro Jara <lautarojra@gmail.com>
2022-12-28 16:23:38 -0300

    agregada clase ListaDeProductos con sus metodos

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as products from "./products.json";
+import * as pull from "lodash/pull";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,76 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    products.map((producto) => {
+      this.addProduct(producto);
+    });
+  }
+  addProduct(product: Product) {
+    const comprueId = this.getCosas().find((productos) => {
+      return productos.id == product.id;
+    });
+    if (comprueId == undefined) {
+      this.add(product);
+    } else {
+      throw new Error("Existen dos o mas productos con mismo Id");
+    }
+  }
+  getProduct(id: number): Product {
+    return this.getCosas().find((producto) => {
+      return producto.id == id;
+    });
+  }
+  removeProduct(id: number): Product {
+    const productoAEliminar = this.getProduct(id);
+    return pull(this.getCosas(), productoAEliminar);
+  }
+  getSortedByPrice(order: string) {
+    const things = this.getCosas();
+    function asc() {
+      things.sort(function (a, b) {
+        if (a.price > b.price) {
+          return 1;
+        }
+        if (a.price < b.price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    function desc() {
+      things.sort(function (a, b) {
+        if (a.price < b.price) {
+          return 1;
+        }
+        if (a.price > b.price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+
+    if (order === "asc") {
+      asc();
+    } else if (order === "desc") {
+      desc();
+    } else {
+      throw console.error("err getSortedByPrice, try 'asc' or 'desc'");
+    }
+    return things;
+  }
+}
+
+function main() {
+  const celu = new Product("samsung 10", 59, 6);
+  const tele = new Product("TV LGBT", 400, 7);
+  const lista = new ListaDeProductos("carrito de compras");
+  /*lista.addProduct(celu);
+  lista.addProduct(tele);
+  console.log(lista);*/
+}
+main();
 
 export { ListaDeProductos, Product };
