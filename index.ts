cec7c2c668e9956a69279f5b430f867a24c49c76
Luis Motoni <luis@debian-BULLSEYE-live-builder-AMD64>
2023-01-25 00:35:33 -0300

    test pasados

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as remove from "lodash/remove";
+import * as orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,31 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    // carga los productos desde un archivo JSON
+    const products = JSON.parse(fs.readFileSync(__dirname + "/products.json").toString());
+    products.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+  // agrega un producto a la lista
+  addProduct(product: Product) {
+    return this.add(product);
+  }
+  // obtiene un producto específico por su id
+  getProduct(id: number): Product {
+    return this.cosas.find((product) => product.id === id);
+  }
+  // elimina un producto por su id
+  removeProduct(id: number): Product {
+    return remove(this.cosas, (p) => p.id == id);
+  }
+  // obtiene la lista de productos ordenada por precio en orden ascendente o descendente
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
