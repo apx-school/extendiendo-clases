b096aa58ceee59c022094cba7a0656a7b6c5740f
hernan87cba <deciccohernan@hotmail.com>
2023-03-05 10:58:23 -0300

    Primer commit de la leccion 9 del módulo 2

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+import * as products from "./products.json";
+import * as _orderBy from "lodash/orderBy";
+import * as _size from "lodash/size";
+import * as _forEach from "lodash/foreach";
+import * as _finder from "lodash/find";
+import * as _remover from "lodash/remove";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +31,75 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(nombreLista: string) {
+    super(nombreLista);
+    //Recorra el json y manda uno por uno a addProduct los productos
+    const productos: Product[] = products;
+    productos.forEach((i) => {
+      this.addProduct(i);
+    });
+  }
+  addProduct(prod: Product) {
+    //si no existe en cosas agregarlo
+    if (_size(this.cosas) > 0) {
+      const encontrado = _finder(this.cosas, function (i) {
+        return i.id === prod.id;
+      });
+      if (!encontrado) {
+        this.add(prod);
+      } else {
+        console.log("No se insertó el producto", prod.name, ". El id", prod.id, "ya existe");
+      }
+    } else {
+      this.add(prod);
+    }
+  }
+  getProduct(id: number): Product {
+    if (_size(this.cosas) > 0) {
+      const encontrado = _finder(this.cosas, function (i) {
+        return i.id === id;
+      });
+      if (encontrado) {
+        return encontrado;
+      } else {
+        console.log("Id " + id + " no encontrado en getProduct");
+      }
+    } else {
+      console.log("Lista sin productos");
+    }
+  }
+  removeProduct(id: number): Product[] {
+    if (_size(this.cosas) > 0) {
+      const encontrado = _remover(this.cosas, function (i) {
+        return i.id === id;
+      });
+      if (encontrado) {
+        return this.cosas;
+      } else {
+        console.log("Id " + id + " no encontrado en removeProduct");
+      }
+    } else {
+      console.log("Lista sin productos");
+    }
+  }
+
+  getSortedByPrice(order: string): Product[] {
+    if (_size(this.cosas) > 0) {
+      if (order !== "asc" && order !== "desc") {
+        console.log("Ordenamiento inválido, ingrese 'asc' o 'desc'");
+      } else {
+        if (order === "asc") {
+          const ordenados: Product[] = _orderBy(this.cosas, "price", order);
+          return ordenados;
+        }
+        if (order === "desc") {
+          const ordenados: Product[] = _orderBy(this.cosas, "price", order);
+          return ordenados;
+        }
+      }
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
