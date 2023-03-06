1775500a9d95742f7efddf178b7e4190c51d684b
Thiago <thiagosalaberry99@gmail.com>
2023-03-06 09:29:21 -0300

    desafío completado

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as remove from "./node_modules/lodash/remove"
+import * as orderBy from "./node_modules/lodash/orderBy"
+import * as fs from "fs"
+import { isMapIterator } from "util/types";
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -23,7 +27,32 @@ class Product {
     this.id = id;
   }
 }
-
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  addProduct(product:Product) {
+    const buscar = this.cosas.find(item => item.id == product.id)
+    if(buscar == undefined) {
+      this.add(product)
+    }
+  }
+  products:Product[]
+  constructor(name:string, products:Product[]) {
+    super(name);
+    const productosJSON = fs.readFileSync(__dirname + "/products.json").toString();
+    const productosDelArchivo = JSON.parse(productosJSON)
+    productosDelArchivo.forEach((item) => {
+      this.addProduct(item)
+    })
+  }
+  getProduct(id:number):Product {
+    const cosas = this.getCosas()
+    return cosas.find((item) => item.id == id)
+  }
+  removeProduct(id:number) {
+    remove(this.cosas, (item) => item.id == id)
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order])
+  }
+}
 
 export { ListaDeProductos, Product };
--- test.ts
+++ test.ts
@@ -14,19 +14,19 @@ test("Test de prueba", (t) => {
 // de cada test
 
 test("Testeo el constructor", (t) => {
-  const lista = new ListaDeProductos("marce");
+  const lista = new ListaDeProductos("marce", products);
   t.is(lista.name, "marce");
 });
 
 test("Testeo que el constructor cargue el products.json", (t) => {
-  const lista = new ListaDeProductos("marce");
+  const lista = new ListaDeProductos("marce", products);
   const cosas = lista.getCosas();
 
   t.deepEqual(cosas, products);
 });
 
 test("Testeo el addProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
+  const lista = new ListaDeProductos("marce",products);
   const myP = { price: 33, id: 123, name: "mi producto" };
   lista.addProduct(myP);
   const myP2 = lista.getProduct(myP.id);
@@ -34,7 +34,7 @@ test("Testeo el addProduct", (t) => {
 });
 
 test("Testeo el removeProduct", (t) => {
-  const lista = new ListaDeProductos("marce");
+  const lista = new ListaDeProductos("marce",products);
   const myP = { price: 33, id: 123, name: "mi producto" };
   lista.addProduct(myP);
   lista.removeProduct(myP.id);
@@ -43,7 +43,7 @@ test("Testeo el removeProduct", (t) => {
 });
 
 test("Testeo el getSortedByPrice", (t) => {
-  const lista = new ListaDeProductos("marce");
+  const lista = new ListaDeProductos("marce",products);
   t.deepEqual(
     orderBy(lista.cosas, "price", "desc"),
     lista.getSortedByPrice("desc")
