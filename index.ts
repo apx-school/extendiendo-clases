fdad23c504b429d4b4a564ba6650042aebe221ea
Marcela <marcelaramireza@gmail.com>
2023-03-19 19:21:26 -0300

    agregué los métodos del desafio

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+var remove = require("lodash.remove");
+var orderBy = require("lodash.orderby");
+import * as fs from "fs";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,34 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+
+    const contenidoDelArchivo = fs
+      .readFileSync(__dirname + "/products.json")
+      .toString();
+    const PorductosDelArchivo = JSON.parse(contenidoDelArchivo);
+
+    PorductosDelArchivo.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+
+  addProduct(product: Product) {
+    return this.add(product);
+  }
+
+  getProduct(id: number) {
+    return this.cosas.find((c) => c.id == id);
+  }
+
+  removeProduct(id: number) {
+    return remove(this.cosas, (n) => (n.id = id));
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], order);
+  }
+}
 
 export { ListaDeProductos, Product };
