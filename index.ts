6b801e9fee78eea8aac58557ac75d84e824c931f
Juan Manuel Montero <juanmanuel231295@gmail.com>
2023-07-17 12:46:50 -0300

    cree y exporte la clase ListaDeProductos y pase los 6 test requeridos

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
+import { readFileSync } from 'node:fs';
+import * as remove from 'lodash/remove';
+import * as reverse from 'lodash/reverse'
+const products=readFileSync("products.json")
+const productsParseados=JSON.parse(products.toString())
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +30,60 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+
+  constructor(name:string){
+    super(name)
+    productsParseados.forEach(element => {
+      this.addProduct(element)
+    });
+      
+    };
+    
+  
+  addProduct(product:Product){
+ this.add(product)
+
+}
+getProduct(id:number):Product{
+  const productFound=this.cosas.find((p=>p.id===id))
+  return productFound
+}
+removeProduct(id:number):any{
+remove(this.cosas, {id: id});
+return this.cosas
+}
+getSortedByPrice(order:"asc"|"desc"){
+  let arrayOrdenado:Product[]
+  
+arrayOrdenado=this.cosas.sort(function (a, b) {
+  if (a.price > b.price) {
+    return 1;
+  }
+  if (a.price < b.price) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});if(order=="asc"){
+  return arrayOrdenado
+}else{
+  return reverse(arrayOrdenado)
+}
+
+
+}
+
+
+}
+// function main(){
+//   const lista =new ListaDeProductos("cosas")
+//    console.log(lista)
+//   lista.addProduct({name:"mazo",price:200, id:8})
+//   console.log(lista)
+  
+// }
+// main()
+
 
 export { ListaDeProductos, Product };
