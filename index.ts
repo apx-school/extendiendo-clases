b4fe0f2487678c6d9437fd8c84c4719fe028b676
Luciano Riente <lucianotomasriente@gmail.com>
2023-10-14 01:58:39 -0300

    Desafio terminado

--- index.ts
+++ index.ts
@@ -1,29 +1,72 @@
+import * as fs from 'fs';
+import { orderBy } from 'lodash';
+
+// const myP2 = { price: 33, id: 123, name: 'mi producto' };
+
 class ListaDeCosas {
-  name: string;
-  cosas: any[] = [];
-  constructor(name: string) {
-    // nombre de esta lista
-    this.name = name;
-  }
-  add(nuevaCosa) {
-    this.cosas.push(nuevaCosa);
-  }
-  getCosas() {
-    return this.cosas;
-  }
+	name: string;
+	cosas: Product[] = [];
+	constructor(name: string) {
+		// nombre de esta lista
+		this.name = name;
+	}
+	add(nuevaCosa: Product) {
+		this.cosas.push(nuevaCosa);
+	}
+	getCosas() {
+		return this.cosas;
+	}
 }
 
 class Product {
-  name: string;
-  price: number;
-  id: number;
-  constructor(name: string, price: number, id: number) {
-    this.name = name;
-    this.price = price;
-    this.id = id;
-  }
+	name: string;
+	price: number;
+	id: number;
+	constructor(name: string, price: number, id: number) {
+		this.name = name;
+		this.price = price;
+		this.id = id;
+	}
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+type Ordenamiento = 'asc' | 'desc';
+
+class ListaDeProductos extends ListaDeCosas {
+	constructor(name: string) {
+		super(name);
+		this.obtenerProductos().forEach((element: Product) => this.addProduct(element));
+	}
+
+	obtenerProductos() {
+		const productos = fs.readFileSync('./products.json');
+		return JSON.parse(productos.toString());
+	}
+
+	addProduct(producto: Product) {
+		const productoIgual = this.getCosas().some((p) => p.id === producto.id);
+		if (!productoIgual) this.add(producto);
+	}
+
+	getProduct(id: number): Product {
+		return this.getCosas().find((p) => p.id === id);
+	}
+
+	removeProduct(id: number): Product {
+		const producto = this.getProduct(id);
+		this.cosas = this.getCosas().filter((p) => p !== producto);
+		return producto;
+	}
+
+	getSortedByPrice(order: Ordenamiento): Product[] {
+		return orderBy(this.getCosas(), 'price', order);
+	}
+}
+
+/* function main() {
+	const nuevaListaDeProductos = new ListaDeProductos('cosita');
+	const myP = { price: 33, id: 123, name: 'mi producto' };
+	// console.log(nuevaListaDeProductos.addProduct(myP));
+}
+main(); */
 
 export { ListaDeProductos, Product };
