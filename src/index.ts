809869c464649d5c686255e6a154e49aebd485c1
Federico Encinaz Sayago <fencinazsayago@frba.utn.edu.ar>
2024-01-02 21:58:53 -0300

    Se finaliza desafio

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
+import fs from "fs";
+import { orderBy } from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
+
   constructor(name: string) {
-    // nombre de esta lista
     this.name = name;
   }
-  add(nuevaCosa) {
+
+  add(nuevaCosa: any) {
     this.cosas.push(nuevaCosa);
   }
-  getCosas() {
+
+  getCosas(): any[] {
     return this.cosas;
   }
 }
@@ -17,6 +22,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +30,42 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(listaName: string) {
+    const productsFile = fs.readFileSync(__dirname + "/products.json");
+    
+    super(listaName);
+    
+    this.cosas = JSON.parse(productsFile.toString());
+  }
+
+  getProduct(idProduct: number): Product | undefined {
+    return this.cosas.find((product) => product.id  === idProduct);
+  }
+
+  addProduct(product: Product): void {
+    const resultSearch =  this.getProduct(product.id);
+
+    if(!resultSearch) this.add(product)
+  }
+
+  removeProduct(idProduct: number): Product | undefined {
+    const indexProductDelete = this.cosas.findIndex((product) => product.id === idProduct);
+    const productDelete = this.cosas[indexProductDelete];
+
+    if(indexProductDelete !== -1) {
+      this.cosas.splice(indexProductDelete, 1);
+
+      return productDelete;
+    }
+
+    return undefined;
+  }  
+
+  getSortedByPrice(order: "asc" | "desc"): Product[] {
+    return orderBy(this.getCosas(), "price", order);
+  }
+
+}
 
 export { ListaDeProductos, Product };
