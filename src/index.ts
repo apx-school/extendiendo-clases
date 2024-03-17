83d1764922777390508a4f0d4139802ef72b9ebc
matiseba2 <87247162+matiseba2@users.noreply.github.com>
2024-03-17 15:17:27 -0300

    fixed functions in ListaDeProductos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import * as fs from "fs";
+import { orderBy, remove } from "lodash";
+
+const productsJSON = fs.readFileSync(__dirname + "./products.json").toString();
+const products = JSON.parse(productsJSON);
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +30,28 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  addProduct(product: Product): void {
+    if (!this.cosas.some((x) => x.id === product.id)) {
+      this.add(product);
+    }
+  }
+  constructor(name: string) {
+    super(name);
+    products.forEach((element) => {
+      this.addProduct(element);
+    });
+  }
+  getProduct(id: number): Product[] {
+    const foundProduct = products.find((product) => product.id === id);
+    return foundProduct;
+  }
+  removeProduct(id: number) {
+    remove(products, (x) => x.id === id);
+  }
+  getSortedByPrice(order: string) {
+    return orderBy(products, order);
+  }
+}
 
 export { ListaDeProductos, Product };
