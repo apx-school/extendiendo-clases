307bc2ff29b0b1fdbef04ed54f20b440f164abd2
elrichardby11 <richardmazuelos@gmail.com>
2024-04-12 23:21:03 -0400

    all the tests passed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as _ from "lodash";
+import { listenerCount } from "process";
+const products = require("./products.json");
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,34 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  productos: object[];
+
+  constructor(name: string) {
+    super(name);
+    const productsJson = products;
+
+    productsJson.forEach((element) => {
+      this.addProduct(element);
+    });
+  }
+  addProduct(product: Product): void {
+    this.add(product);
+  }
+
+  getProduct(id: number): Product {
+    return this.getCosas().find((p) => p.id == id);
+  }
+  removeProduct(id: number): Product | undefined {
+    const index = _.findIndex(this.cosas, (p) => p.id == id);
+    if (index !== 1) {
+      return this.cosas.splice(index, 1)[0];
+    }
+    return undefined;
+  }
+  getSortedByPrice(order: "asc" | "desc") {
+    return _.orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
