b61f6a01179e053ee9bde5ab83dc92f7d756a1a6
Adrian Bahia <bahiaadrian@gmail.com>
2024-05-10 20:19:40 -0300

    primera correccion

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import products from "./products.json";
+import { orderBy, remove } from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,58 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  cosas: Product[] = [];
+  constructor(nombre: string) {
+    // Llamada al constructor de la superclase
+    super(nombre);
+
+    // Lógica adicional para leer products.json y agregar productos usando addProduct
+    // ...
+    this.cosas = products;
+    // Ejemplo de cómo se puede invocar addProduct para agregar un producto
+    // this.addProduct(new Product(/* parámetros del producto */));
+  }
+
+  addProduct(product: Product): void {
+    // Implementación del método addProduct
+    // ...
+    this.cosas.push(product);
+  }
+
+  getProduct(id: number): Product {
+    // Implementación del método getProduct
+    // ..
+    const productoBuscado = this.cosas.find((cosas) => cosas.id == id);
+    return productoBuscado;
+  }
+
+  removeProduct(id: number): Product {
+    // Implementación del método removeProduct
+    // ...
+    return remove(this.cosas, (cosas) => cosas.id == id);
+  }
+
+  getSortedByPrice(order: string): void {
+    // Implementación del método getSortedByPrice
+    // ...
+    return (this.cosas = orderBy(this.cosas, ["price"], [order]));
+  }
+}
 
 export { ListaDeProductos, Product };
+
+//const lista = new ListaDeProductos("listita");
+// console.log(lista);
+// const unoPorId = lista.getProduct(2);
+// console.log(unoPorId);
+// lista.removeProduct(2);
+// console.log(lista);
+// lista.getSortedByPrice("desc");
+// console.log(lista);
+// lista.getSortedByPrice("asc");
+// console.log(lista);
+
+//const myP = { price: 33, id: 123, name: "mi producto" };
+// lista.addProduct(myP);
+// console.log(lista);
