c7e137f89c578e8a1df05eda0492a7f02c8de8a2
lucacarena97 <lucacarena98@gmail.com>
2024-06-24 12:08:26 -0300

    test passed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import remove from "lodash/remove";
+import orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,41 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    // Llamada al constructor de la superclase: LisaDeCosas
+    super(n);
+    // Lógica adicional para leer products.json y agregar productos usando addProduct
+    // Ejemplo de cómo se puede invocar addProduct para agregar un producto
+    const json = fs.readFileSync(__dirname + "/products.json").toString();
+    const productos = JSON.parse(json);
+    productos.forEach((e) => {
+      this.addProduct(e);
+    });
+  }
+
+  addProduct(product: Product): void {
+    const exists = this.cosas.find((cosa) => cosa.id === product.id);
+
+    if (!exists) {
+      this.add(product);
+    } else {
+      console.log(`El producto con ID ${product.id} ya existe.`);
+    }
+  }
+
+  getProduct(id: number): Product {
+    const cosas = this.cosas;
+    return cosas.find((cosa) => cosa.id === id);
+  }
+
+  removeProduct(id: number) {
+    remove(this.cosas, (c) => c.id === id);
+  }
+
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price", [order]]);
+  }
+}
 
 export { ListaDeProductos, Product };
