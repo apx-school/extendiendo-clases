73da42fd63212ecf18e3c41ce7b71d8d8325b179
Carlos Mario <carlosmario@gmail.com>
2025-01-29 17:27:56 -0500

    All methods creatd, tests passed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import products from "./products.json";
+import { orderBy } from "lodash";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -9,7 +12,7 @@ class ListaDeCosas {
     this.cosas.push(nuevaCosa);
   }
   getCosas() {
-    return this.cosas;
+    return this.cosas; // .map((p) => ({ id: p.id, name: p.name, price: p.price }));
   }
 }
 
@@ -24,6 +27,51 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+
+    //* Solución corta:
+    /* products.forEach((p) => {
+      this.addProduct(p);
+    }); */
+
+    //* Mi solución:
+    for (let i = 0; i < products.length; i++) {
+      const id = products[i].id;
+      const nombre = products[i].name;
+      const precio = products[i].price;
+      const nuevoProducto = new Product(nombre, precio, id);
+      const nuevoProductoFormateado = {
+        id: nuevoProducto.id,
+        name: nuevoProducto.name,
+        price: nuevoProducto.price,
+      };
+      this.addProduct(nuevoProductoFormateado);
+    }
+  }
+  addProduct(product: Product): void {
+    this.add(product);
+  }
+
+  getProduct(id: number): Product {
+    return this.cosas.find((p) => p.id == id);
+  }
+
+  removeProduct(id: number) {
+    const i = this.cosas.findIndex((p) => p.id === id);
+    if (i !== -1) {
+      this.cosas.splice(i, 1);
+    }
+  }
+
+  getSortedByPrice(order: "asc" | "desc"): void {
+    if (order == "asc") {
+      return orderBy(this.cosas, "price", "asc");
+    } else if (order == "desc") {
+      return orderBy(this.cosas, "price", "desc");
+    }
+  }
+}
 
 export { ListaDeProductos, Product };
