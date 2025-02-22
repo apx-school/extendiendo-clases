950a531945ac04e79c6e198a04391ba1039d5049
JhonatanDzG <jhonatandzg@gmail.com>
2025-02-22 18:54:04 -0500

    test: all-test-passed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import fs from "fs";
+import {remove}  from "lodash"; 
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +27,39 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    // Llamada al constructor de la superclase: ListaDeCosas
+    super(n);
+    const contenido = fs.readFileSync(__dirname + '/products.json', 'utf-8').toString();
+    const productsFile = JSON.parse(contenido);
+    productsFile.forEach((p) => {
+      this.addProduct(p);
+    });
+
+  }
+
+  addProduct(product: Product): void {
+    // Implementación del método addProduct
+    this.add(product);
+    
+    
+  }
+
+  getProduct(id: number): Product {
+    // Implementación del método getProduct
+    return this.cosas.find((c)=> c.id === id);
+  }
+
+  removeProduct(id: number) {
+    // Implementación del método removeProduct
+    remove(this.cosas, ((c) => c.id == id));
+  }
+
+  getSortedByPrice(order: string): Product[] {
+    // Implementación del método getSortedByPrice
+     return this.cosas.sort((a,b)=> order === "asc" ? a.price - b.price : b.price - a.price);
+  }
+}
 
 export { ListaDeProductos, Product };
