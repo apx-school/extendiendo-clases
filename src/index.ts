d0b99ee69cf5e654a3e996edf25c1004f432cb23
sebas777 <sebastiancast06@hotmail.com>
2025-03-11 13:01:16 -0500

    [sc]feat:extendiendo clases

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import{ readFileSync } from "fs";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,65 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name:string) {
+    super(name);
+
+  const data = readFileSync("./products.json","utf-8");
+  const products = JSON.parse(data);
+  
+
+  products.forEach((producto:Product) =>{
+      this.addProduct(producto);
+  });
+  
+  }
+  
+  //Implementacion del metodo addProduct
+  addProduct(producto:Product) : void {
+
+    //verificar sielproducto ya existe 
+    const exists = this.cosas.some((cosa)=> cosa.id === producto.id);
+  
+    if(!exists){
+      this.add(producto);//Agregarelproducto si no existe
+    } else{
+      console.log(`Elproducto con ID ${producto.id} ya existe`);
+    }
+  } 
+
+  getProduct(id:number):Product{
+    return this.cosas.find((producto)=> producto.id ===id);
+  }  
+
+  removeProduct(id:number):Product{
+    const index = this.cosas.findIndex((producto)=>producto.id === id);
+    if(index!==-1){
+      return this.cosas.splice(index,1)[0];
+    }else{
+      console.log(`Elproducto con ID ${id} NO existe`);
+    }
+  }
+
+  getSortedByPrice(order:string):any[]{
+    // Copiar la lista para no modificarla la original 
+    const sortedList = [...this.cosas];
+
+    // Ordenar la lista segun precio
+    sortedList.sort((a:Product, b:Product) =>{
+      if(order === "asc"){
+        return a.price - b.price;// Ascendente
+      }else{
+        return b.price - a.price;// Descendente
+      }
+      
+
+    });
+
+    return sortedList;
+   
+  }
+
+}
 
 export { ListaDeProductos, Product };
