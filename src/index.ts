aeb5c0a9bb584342a7a264b79dcd963b7d669712
Mariano <mariano@pop-os.localdomain>
2025-03-17 20:14:28 -0300

    Se implementaron los métodos y funciones necesarios para pasar los tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import fs from "fs";
+import remove from "lodash/remove";
+import orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +28,32 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    super(n);
+    const productos = fs.readFileSync(__dirname + "/products.json").toString();
+    const productosDelArchivo = JSON.parse(productos);
+
+    productosDelArchivo.forEach((p) => {
+      this.addProduct(p);
+    });
+  }
+
+  addProduct(product: Product) {
+    this.add(product);
+  }
+  getProduct(id: number): Product {
+    const cosas = this.getCosas();
+    return cosas.find((p) => p.id == id);
+  }
+
+  removeProduct(id: number) {
+    remove(this.cosas, (c) => c.id == id);
+  }
+
+  getSortedByPrice(order: "asc" | "desc") {
+    return orderBy(this.cosas, ["price"], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
