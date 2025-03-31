ca4125033d83f3213a37c6267edc56daaf3bd271
carlosaac23 <carlosaac23@hotmail.com>
2025-03-31 17:52:40 -0500

    resuelto el index.ts para que pase el test
    se agregaron funciones como addProduct, getProduct, removeProduct y getSortedByPrice al igual que se lee mediante readFileSync products.json para agregar esos productos

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import { orderBy } from 'lodash';
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
+
   constructor(name: string) {
     // nombre de esta lista
     this.name = name;
   }
+
   add(nuevaCosa) {
     this.cosas.push(nuevaCosa);
   }
+
   getCosas() {
     return this.cosas;
   }
@@ -17,6 +24,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +32,28 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(name: string) {
+    super(name);
+    const productsJSON = JSON.parse(readFileSync(join(__dirname + '/products.json'), 'utf-8'));
+    productsJSON.forEach(product => this.addProduct(product));
+  }
+
+  addProduct(product: Product): void {
+    this.add(product);
+  }
+
+  getProduct(id: number): Product {
+    return this.cosas.find(producto => producto.id === id);
+  }
+
+  removeProduct(id: number): Product[] {
+    return (this.cosas = this.cosas.filter(cosa => cosa.id !== id));
+  }
+
+  getSortedByPrice(order: 'asc' | 'desc'): void {
+    return orderBy(this.cosas, ['price'], [order]);
+  }
+}
 
 export { ListaDeProductos, Product };
