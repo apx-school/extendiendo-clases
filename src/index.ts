00384224a13324ec41d788098f6fa7d44aad0bc7
NikolasGnzlz <nikolasgnzlz@gmail.com>
2025-04-15 14:23:38 -0300

    complete desafio

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import * as fs from "fs";
+import * as path from 'path';
+import  remove from "lodash/remove";
+import  orderBy from "lodash/orderBy";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
-  constructor(name: string) {
-    // nombre de esta lista
+
+  constructor(name: string, cosas: any[]) {
     this.name = name;
+    this.cosas = cosas;
   }
   add(nuevaCosa) {
     this.cosas.push(nuevaCosa);
@@ -17,6 +23,7 @@ class Product {
   name: string;
   price: number;
   id: number;
+
   constructor(name: string, price: number, id: number) {
     this.name = name;
     this.price = price;
@@ -24,6 +31,62 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+constructor(name : string){
+  super(name,[]);
+
+  const contenidoDelArchivo = fs.readFileSync(path.join(__dirname, "products.json")).toString();
+  const productosDelArchivo = JSON.parse(contenidoDelArchivo);
+
+  if (Array.isArray(productosDelArchivo)) {
+  productosDelArchivo.forEach((p) => {
+    
+    this.add(p);
+  
+  });
+} else {
+  console.error('El contenido de products.json no es un array.');
+}
+}
+
+  addProduct(product: Product){
+    this.add(product);
+}
+
+getProduct(id :number): Product{
+  const cosas = this.getCosas();
+  return cosas.find((c) => c.id === id);
+}
+
+removeProduct(id : number): boolean {
+  const initialLength = this.cosas.length; // Longitud inicial
+  remove(this.cosas, (c) => c.id === id); // Elimina el producto
+  return this.cosas.length < initialLength; // Retorna true si se eliminó
+}
+
+
+
+getSortedByPrice(order: "asc" | "desc"){
+  return orderBy(this.cosas, ["price"], [order]);
+  
+}
+}
 
 export { ListaDeProductos, Product };
+
+
+
+/*
+1. Error en el Test de Carga de products.json
+El primer error indica que el test espera un array de objetos simples, pero está recibiendo instancias de la clase Product. Esto se debe a que el test está comparando los objetos 
+en su forma original.
+
+Solución
+Para que el test pase, puedes ajustar la forma en que se están agregando los productos. En lugar de crear instancias de Product, podrías simplemente agregar los objetos directamente. 
+Sin embargo, esto no es lo ideal si quieres mantener la lógica de tu aplicación.
+
+productosDelArchivo.forEach((p) => {
+  this.add(p); // Agrega el objeto directamente
+});
+
+*/
