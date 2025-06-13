1494a59ef49dfe7d1e15c1c20aa3571ed2a8b56f
Hernan <deciccohernan@gmail.com>
2025-06-13 14:23:18 -0300

    Funciones de la clase ListaDeProductos completas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import products from "./products.json";
+
 class ListaDeCosas {
   name: string;
   cosas: any[] = [];
@@ -24,6 +26,54 @@ class Product {
   }
 }
 
-class ListaDeProductos extends ListaDeCosas {}
+class ListaDeProductos extends ListaDeCosas {
+  constructor(n: string) {
+    // Llamada al constructor de la superclase: LisaDeCosas
+    super(n);
+
+    // Lógica adicional para leer products.json y agregar productos usando addProduct
+    // ...
+    // Ejemplo de cómo se puede invocar addProduct para agregar un producto
+    // this.addProduct(new Product(/* parámetros del producto */));
+    for (let p of products) {
+      this.addProduct(p);
+    }
+  }
+
+  addProduct(product: Product): void {
+    // Implementación del método addProduct
+    // ...
+    const encontrado = this.cosas.includes((p) => p.id === product.id);
+    if (!encontrado) {
+      this.cosas.push(product);
+    }
+  }
+
+  getProduct(id: number): Product {
+    // Implementación del método getProduct
+    // ...
+    const encontrado = this.cosas.find((p) => p.id === id);
+    if (encontrado !== undefined) return encontrado;
+  }
+
+  removeProduct(id: number): void {
+    // Implementación del método removeProduct
+    // ...
+    const listaFiltrada = this.cosas.filter((p) => p.id !== id);
+    this.cosas = listaFiltrada;
+  }
+
+  getSortedByPrice(order: string): Product[] {
+    // Implementación del método getSortedByPrice
+    // ...
+    let listaOrdenada = this.cosas;
+    if (order === "asc") {
+      listaOrdenada.sort((a, b) => a.price - b.price);
+    } else {
+      listaOrdenada.sort((a, b) => b.price - a.price);
+    }
+    return listaOrdenada;
+  }
+}
 
 export { ListaDeProductos, Product };
